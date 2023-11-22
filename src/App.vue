520744e284392b932c72cb5fcc8fcb7322b2ef93
romycode <albert.romera.arm@gmail.com>
2023-11-22 13:41:41 +0100

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="sidebar"></aside>
+  <main class="client">
+    <section class="request">
+      <section></section>
+      <section></section>
+      <section></section>
+    </section>
+    <section class="response">
+      <section></section>
+      <section></section>
+    </section>
+  </main>
+</template>
+
+<style>
+#app {
+  display: grid;
+
+  gap: var(--spacing);
+  grid-template-rows: 1fr;
+  grid-template-columns: 0.25fr 0.75fr;
+  grid-template-areas: 'sidebar client';
+
+  width: 100%;
+  height: 100%;
+
+  overflow: hidden;
+
+  & > .sidebar {
+    display: flex;
+    grid-area: sidebar;
+
+    gap: var(--spacing);
+
+    flex-direction: row;
+  }
+
+  & > .client {
+    grid-area: client;
+  }
+}
+</style>
